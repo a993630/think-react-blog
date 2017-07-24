8732f81699b98bf00c3110afeab6a5c1c021fec3
liuliming <694467256@qq.com>
2017-07-24 16:24:39 +0800

    增加layout

--- client/front/global.jsx
+++ client/front/global.jsx
@@ -2,17 +2,16 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { Router, Route, Link, IndexRoute } from 'react-router'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
-
+import Layouts from '../components/layout/index';
 import Index from '../container/index'
-
 
 const history = createBrowserHistory();
 
 
 ReactDOM.render(
 	<Router history={history}>
-		<Route path="/">
-			<IndexRoute component={Index} />
+		<Route component={Layouts}>
+			<Route component={Index} path="/"/>
 		</Route>
 	</Router>,
 	document.getElementById('example')
